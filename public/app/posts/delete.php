5cadde59c5092587928bd97f4aca3c4622973d48
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-01-12 13:23:20 +0100

    added root directory

--- public/app/posts/delete.php
+++ public/app/posts/delete.php
@@ -16,9 +16,6 @@ if (!isset($_SESSION["user"]) || $_SESSION["authenticated"] !== true) {
     $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
     $statement->execute();
 
-    // $statement = $database->query('SELECT * FROM posts WHERE id = :post_id');
-    // $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
     $_SESSION['message'] = 'Your post has been deleted.';
 
     redirect('/submitted.php');
