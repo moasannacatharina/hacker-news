cfb21fde105552d45a80bb58773dbdceb3dc14f6
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-01-13 21:00:18 +0100

    Update logout.php

--- public/app/users/logout.php
+++ public/app/users/logout.php
@@ -9,7 +9,6 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_SESSION['user'])) {
     unset($_SESSION['user']);
-    session_destroy();
 }
 $_SESSION['message'] = "You have been logged out.";
 redirect('/login.php');
